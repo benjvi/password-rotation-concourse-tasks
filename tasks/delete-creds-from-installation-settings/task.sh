b02186c61b00f899cd82eee3291f4c56351c4898
Ben Vickers <bvickers@pivotal.io>
2019-09-25 18:56:46 +0200

    get + get first two tasks working

--- tasks/delete-creds-from-installation-settings/task.sh
+++ tasks/delete-creds-from-installation-settings/task.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-CREDENTIAL_IDS="director_credentials"
-TILE_INSTALLATION_NAME="p-bosh"
-JOB_INSTALLATION_NAME="director"
-
-
+echo "Credential IDs: $CREDENTIAL_IDS"
 cd installation_settings_input
 
-for credential_id in $CREDENTIAL_IDS; do
-  cat installation_settings.json | jq "del(.products[]| select(.installation_name == \"$TILE_INSTALLATION_NAME\")| .jobs[] | select(.installation_name == \"$JOB_INSTALLATION_NAME\")| .properties[] | select(.identifier == "$credential_id"))" > installation_settings.json
+for credential_id in ${CREDENTIAL_IDS}; do
+  echo "Removing: $credential_id"
+  cat installation_settings.json | jq "del(.products[]| select(.installation_name == \"$TILE_INSTALLATION_NAME\")| .jobs[] | select(.installation_name == \"$JOB_INSTALLATION_NAME\")| .properties[] | select(.identifier == \"$credential_id\"))" > tmp
+  cat tmp > installation_settings.json
   set +e
   cat installation_settings.json | grep -n $credential_id && echo "WARNING: $credential_id still exists in installation settings"
   set -e
 done
 
-mv installation_settings.json ../installation_settings_filtered/installation_settings.json
+cp installation_settings.json ../installation_settings_output/installation_settings.json
 
